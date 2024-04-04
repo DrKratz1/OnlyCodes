e949ea0ea2fbfc8b9c1ad9b8e514c19a03d04930
chenhzhang2 <winston.zhang2@student.unimelb.edu.au>
2024-04-04 16:01:16 +1100

    unscreweing us

--- studyroom/tailwind.config.js
+++ studyroom/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{jsx,js}"],
+  content: ["./templates/index.html", "./src/**/*.{jsx,js}"],
   darkmode: "class",
   theme: {
     extend: {
