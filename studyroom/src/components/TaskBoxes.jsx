6c572a50dc506c2becd5baf13985fd2da0403012
K1mKardashian <benzhou09@gmail.com>
2024-04-05 13:22:37 +1100

    Update TaskBoxes.jsx
    updating

--- studyroom/src/components/TaskBoxes.jsx
+++ studyroom/src/components/TaskBoxes.jsx
@@ -1,7 +1,12 @@
 import React from "react"
 
 function TaskBoxes() {
-  return <div>Test for TaskBoxes</div>
+  return (
+    <button class="cursor-pointer animate-bounce px-10 pt-5 pb-5 rounded-full text-white text-xl 
+    bg-gradient-to-r from-cyan-500 to-blue-500 hover:border-cyan-500 hover:shadow-2xl">
+    Button
+    </button>
+  )
 }
 
 export default TaskBoxes
