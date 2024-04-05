a4f5baead4a4d7bcba6ac6fbd595189a47f4ef0b
K1mKardashian <benzhou09@gmail.com>
2024-04-05 16:13:55 +1100

    NO MORE BOUNCY  BOY

--- studyroom/src/components/TaskBoxes.jsx
+++ studyroom/src/components/TaskBoxes.jsx
@@ -4,8 +4,7 @@ function TaskBoxes() {
   return (
     <div className = "h-screen flex items-center justify-center">
       <a href = "/NewTask">
-        <button class="cursor-pointer px-20 h-96 rounded text-white text-xl font-extrabold
-        hover:animate-bounce">
+        <button class="cursor-pointer px-20 h-96 rounded text-white text-xl font-extrabold">
         NEW TASK
         <img src="/assets/TaskPic1.jpeg" alt = "TaskPic1" width = "200"
         className="rounded-md outline-3 outline-blue-950"/>
