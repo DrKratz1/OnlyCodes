d8ad69ef9590e509bec8611a6e917f53ceba90d5
K1mKardashian <benzhou09@gmail.com>
2024-04-05 15:50:05 +1100

    Updated pics and taskboxes

--- studyroom/src/components/TaskBoxes.jsx
+++ studyroom/src/components/TaskBoxes.jsx
@@ -4,9 +4,12 @@ function TaskBoxes() {
   return (
     <div className = "h-screen flex items-center justify-center">
       <a href = "https://www.youtube.com/watch?v=b0pkpcD8Ms4">
-        <button class="cursor-pointer px-20 pt-5 pb-5 h-64 rounded text-white text-xl 
-        bg-gradient-to-r from-cyan-500 bg-blue-900 hover:animate-bounce">
+        <button class="cursor-pointer px-20 h-96 rounded text-white text-xl font-extrabold
+        hover:animate-bounce">
         NEW TASK
+        <img src="/assets/TaskPic1.jpeg" alt = "TaskPic1" width = "200"
+        className="rounded-md outline-3 outline-blue-950"/>
+        
         </button>
     </a>
       
